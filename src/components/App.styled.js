41d8165bcab6c9dc2df40ecce3eca10437161dba
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-05-10 22:09:02 +0300

    phonebook

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -3,9 +3,11 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
      height: '100vh';
      text-align: center;
-        display: 'flex';
-        justify-content: 'center';
         align-items: 'center';
-        font-size: 40;
-        color: '#010101;
+        background-color: aquamarine;
+        flex-direction: column;
+        display: flex;
+        margin: 0 auto;
+    width: 100%;
+    padding-bottom: 30px;
 `;
