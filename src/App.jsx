6468631334f3d4c9423b986c36d5ce1dad03ba45
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-05-11 18:23:18 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./App.styled";
+import FeedBack from './FeedBack/FeedBack'
+
+export const App = () => {
+  return (
+    <Container>
+      <FeedBack></FeedBack>
+    </Container>
+  );
+};
